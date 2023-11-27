f2fda5a9c81bdd2f0ad2d25bcf5d534e6f1bdeb6
Md. Abid Raihan <abidcsedu@gmail.com>
2023-11-27 19:01:20 +0600

    Integrate redux-toolkit

--- src/components/TaskList.stories.jsx
+++ src/components/TaskList.stories.jsx
@@ -1,25 +1,25 @@
 import TaskList from "./TaskList";
 import * as TaskStories from "./Task.stories";
 
-export default {
-	component: TaskList,
-	title: "TaskList",
-	decorators: [(story) => <div style={{ padding: "3rem" }}>{story()}</div>],
-	tags: ["autodocs"],
+import { Provider } from "react-redux";
+
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+// A super-simple mock of the state of the store
+export const MockedState = {
+	tasks: [
+		{ ...TaskStories.Default.args.task, id: "1", title: "Task 1" },
+		{ ...TaskStories.Default.args.task, id: "2", title: "Task 2" },
+		{ ...TaskStories.Default.args.task, id: "3", title: "Task 3" },
+		{ ...TaskStories.Default.args.task, id: "4", title: "Task 4" },
+		{ ...TaskStories.Default.args.task, id: "5", title: "Task 5" },
+		{ ...TaskStories.Default.args.task, id: "6", title: "Task 6" },
+	],
+	status: "idle",
+	error: null,
 };
 
-export const Default = {
-	args: {
-		tasks: [
-			{ ...TaskStories.Default.args.task, id: "1", title: "Task 1" },
-			{ ...TaskStories.Default.args.task, id: "2", title: "Task 2" },
-			{ ...TaskStories.Default.args.task, id: "3", title: "Task 3" },
-			{ ...TaskStories.Default.args.task, id: "4", title: "Task 4" },
-			{ ...TaskStories.Default.args.task, id: "5", title: "Task 5" },
-			{ ...TaskStories.Default.args.task, id: "6", title: "Task 6" },
-		],
-	},
-};
+// A super-simple mock of a redux store
 
 export const WithPinnedTask = {
 	args: {
